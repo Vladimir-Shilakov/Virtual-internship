533db1f6052f90ab8dc76b415e1fa68b7361352c
Vladimir-Shilakov <vladimir.shilakov@gmail.com>
2023-10-24 15:13:13 +0300

    Созданию вьюсетов

--- Mountains/pereval/views.py
+++ Mountains/pereval/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from .serializers import *
+from rest_framework import viewsets
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class CoordViewSet(viewsets.ModelViewSet):
+    queryset = Coords.objects.all()
+    serializer_class = CoordsSerializer
+
+
+class LevelViewSet(viewsets.ModelViewSet):
+    queryset = Level.objects.all()
+    serializer_class = LevelSerializer
+
+
+class ImagesViewSet(viewsets.ModelViewSet):
+    queryset = Images.objects.all()
+    serializer_class = ImagesSerializer
+
+
+class MountainViewSet(viewsets.ModelViewSet):
+    queryset = Mountain.objects.all()
+    serializer_class = MountainSerializer
