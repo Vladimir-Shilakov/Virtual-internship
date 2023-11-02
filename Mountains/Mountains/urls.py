43398af57aa99302bb9c808a5395c733f3a6cfe7
Vladimir-Shilakov <130110318+Vladimir-Shilakov@users.noreply.github.com>
2023-11-02 23:11:30 +0300

    Добавление Swagger

--- Mountains/Mountains/urls.py
+++ Mountains/Mountains/urls.py
@@ -2,21 +2,34 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework import routers
+from rest_framework import routers, permissions
 from pereval import views
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Products API",
+      default_version='v1',
+      description="Description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 router = routers.DefaultRouter()
-router.register(r'user', views.UserViewSet)
-router.register(r'coords', views.CoordViewSet)
-router.register(r'levels', views.LevelViewSet)
-router.register(r'images', views.ImagesViewSet)
 router.register(r'submitData', views.MountainViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 ]
 
 if settings.DEBUG:
@@ -24,5 +37,4 @@ if settings.DEBUG:
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT
     )
-
 
